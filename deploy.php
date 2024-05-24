5431a2a0f22c4d804df798965e50ca10f5395d56
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-05-24 19:18:13 +0200

    deploy.php configured

--- deploy.php
+++ deploy.php
@@ -5,18 +5,34 @@ require 'recipe/laravel.php';
 
 // Config
 
+set('application', 'backoffice');
 set('repository', 'https://github.com/FranGre/backoffice.git');
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+add('shared_files', ['.env', 'database/database.sqlite']);
+add('shared_dirs', ['bootstrap/cache', 'storage']);
+add('writable_dirs', ['bootstrap/cache', 'storage']);
 
 // Hosts
 
 host('54.208.47.115')
-    ->set('remote_user', 'deployer')
-    ->set('deploy_path', '~/backoffice');
+    ->set('remote_user', 'backoffice-deployer')
+    ->set('identity_file', '~/.ssh/id_rsa')
+    ->set('deploy_path', '/home/backoffice-deployer/var/www');
+
+set('keep_releases', 3);
+
+// Taks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+task('reload:php-fpm', function(){
+    run('sudo /etc/init.d/php8.3-fpm restart')
+});
 
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
+// before('deploy:symlink', 'artisan:migrate');
+after('deploy', 'reload:php-fpm');
